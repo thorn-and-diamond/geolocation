ef1e01b77387a4f26c0adf9f8b1887e55b3d75c1
Alitsiya <aliaj85@gmail.com>
2015-10-29 22:26:20 -0700

    combined janica's and mine scripts, final index.html

--- getWeather.js
+++ getWeather.js
@@ -1,94 +1,88 @@
 
-var testmode= true;
-var currLong = 0;
-var currLat = 0;
-var response = {};
-var loc = {};
-var allResponse = {};
+var getMain = function(clong,clat)
+{
 
-var displayHead = function(){
-                        var wday = loc.observation_time_rfc822.split(" ") ;
-                        var ltime = loc.observation_time.split(" ");
-                        var myOutPut = "<h2>" + loc.display_location.full + 
-                        "</h2><h3>" + wday[0] + " " + ltime[5] + " " +
-                        ltime[6] + "</h3><h3>" + loc.weather + "</h3>";
-
-                       $('#head').html(myOutPut);
-
-};
-
-var displayToday = function(){
-                    var myOutPut = "<img src=" +'"' + loc.icon_url + '"' + "/>" + 
-                    " " + loc.temp_f +"<sup>" +"o</sup><sup >F</sup>";
-                    
-                    $('#today').html(myOutPut);
-};
-
-var displayTodayR = function(){
-                    myOutPut = "<div class='right'><p>Precipitation: " + response[0].pop + 
-                    "%</p><p>Humidity: " + loc.relative_humidity + "</p><p>Wind: " + loc.wind_mph
-                    + " mph</p>";
-
-                  $('#todayR').html(myOutPut);
-};
-
-var getForeCast = function(num){
-      if (num === response.length) return;
-      var tmpdiv = "#day" + num ;
-      var outStr = "<p>" + response[num].date.weekday_short + "</p><p><img src=" +
-                          '"' + response[num].icon_url + '"' +"/></p><p>" + response[num].high.fahrenheit +
-                          " " + response[num].low.fahrenheit + "</p>";
-                          $(tmpdiv).html(outStr);
-                          getForeCast(num+1);
-};
+    var response = {};
+    var loc = {};
 
 
-function getLocation() {
-    if (testmode){
-      getLocWeather();
-    }else if (navigator.geolocation) {
-      
-       navigator.geolocation.getCurrentPosition(getLocWeather);
-    } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";
+    var displayHead = function () {
+        var wday = loc.observation_time_rfc822.split(" ");
+        var ltime = loc.observation_time.split(" ");
+        var myOutPut = "<h2>" + loc.display_location.full +
+            "</h2><h3>" + wday[0] + " " + ltime[5] + " " +
+            ltime[6] + "</h3><h3>" + loc.weather + "</h3>";
+
+        $('#head').html(myOutPut);
+
+    };
+
+    var displayToday = function () {
+        var myOutPut = "<img src=" + '"' + loc.icon_url + '"' + "/>" +
+            "<div id='temp'>" + loc.temp_f + "</div><sup class='degree'>" +
+            "o</sup>";
+
+        $('#today').html(myOutPut);
+    };
+
+    var displayTodayR = function () {
+        myOutPut = "<div><p>Precipitation: <b>" + response[0].pop +
+            "%</b>   Humidity: <b>" + loc.relative_humidity + "  </b><em>Wind:<b> " + loc.wind_mph
+            + " mph</b></em></p>";
+
+        $('#todayR').html(myOutPut);
+    };
+
+    var getForeCast = function (num) {
+        if (num === response.length) return;
+        var tmpdiv = "#day" + num;
+        var outStr = "<div class='threeday'><p>" + response[num].date.weekday_short + "</p><p><img src=" +
+            '"' + response[num].icon_url + '"' + "/></p><p>H:" + response[num].high.fahrenheit +
+            " L:" + response[num].low.fahrenheit + "</p></div>";
+        $(tmpdiv).html(outStr);
+        getForeCast(num + 1);
+    };
+
+
+    function getLocWeather() {
+
+        $.ajax({
+
+            url: "https://api.wunderground.com/api/6887efd8390c8e5e/forecast/q/" +
+            clat + "," + clong + ".json",
+            dataType: "jsonp",
+            success: function (parsed_json) {
+                response = parsed_json['forecast']['simpleforecast']['forecastday'];
+                err1 = parsed_json;
+            }
+        });
+
+        var doItAllNow = function () {
+            $.ajax({
+
+                url: "https://api.wunderground.com/api/6887efd8390c8e5e/conditions/q/" +
+                clat + "," + clong + ".json",
+                dataType: "jsonp",
+                success: function (parsed_json) {
+                    loc = parsed_json['current_observation'];
+                    err2 = parsed_json;
+
+                    displayHead();
+                    displayToday();
+                    displayTodayR();
+                    getForeCast(1);
+                }
+            });
+        }
+
+        setTimeout(function () {
+            doItAllNow()
+        }, 2000);
+
+
     }
+
+    getLocWeather();
 }
-
-function getLocWeather(position) {
-    currLong = -121.30594980000001;
-    currLat = 38.874983;
- 
-    $.ajax({
-              
-          url : "https://api.wunderground.com/api/6887efd8390c8e5e/forecast/q/" + 
-                   currLat  + "," + currLong + ".json",
-          dataType : "jsonp",
-          success : function(parsed_json) {
-            response = parsed_json['forecast']['simpleforecast']['forecastday'];
-            err1 = parsed_json; 
-          }
-    });   
-
-var doItAllNow = function(){
-  $.ajax({
-            
-          url : "https://api.wunderground.com/api/6887efd8390c8e5e/conditions/q/" + 
-                   currLat  + "," + currLong + ".json",
-          dataType : "jsonp",
-          success : function(parsed_json) {
-              loc = parsed_json['current_observation']; 
-              err2 = parsed_json;
-              
-              displayHead();
-              displayToday();
-              displayTodayR();
-              getForeCast(1);
-          }
-    });
-}
-
-      setTimeout(function(){doItAllNow()},2000);
- 
-
 
 
--- weatherAccessories.js
+++ weatherAccessories.js
@@ -1,36 +1,3 @@
-    var x = document.getElementById("demo");
-
-    function getLocation() {
-        if (navigator.geolocation) {
-            /* navigator.geolocation.getCurrentPosition(showPosition);*/
-            navigator.geolocation.getCurrentPosition(getWeather);
-        } else {
-            x.innerHTML = "Geolocation is not supported by this browser.";
-        }
-    }
-
-    function getWeather(position) {
-
-        x.innerHTML = "Latitude: " + position.coords.latitude +
-            "<br>Longitude: " + position.coords.longitude;
-        jQuery(document).ready(function($) {
-            $.ajax({
-                url: "https://api.wunderground.com/api/6887efd8390c8e5e/geolookup/conditions/q/" +
-                    position.coords.latitude + "," + position.coords.longitude + ".json",
-                dataType: "jsonp",
-                success: function(parsed_json) {
-                    var city = parsed_json['location']['city'];
-                    var st = parsed_json['location']['state'];
-                    var temp_f = parsed_json['current_observation']['temp_f'];
-                    var weather = parsed_json['current_observation']['weather'];
-
-                    alert("City: " + city + " State: " + st + " Temp: " + temp_f + " Weather: " + weather);
-                    $('#todaysWeather').append("City: " + city + " State: " + st + " Temp: " + temp_f + " Weather: " + weather);
-                    $('#whatToWear').append(weatherAccessories(weather));
-                }
-            });
-        });
-    }
     //function that loads additional acsesories depending on weather (rain, sunny etc.)
     var weatherAccessories = function(weatherStr) {
         var weatherArr = {
@@ -124,3 +91,85 @@
             }
         }
     };
+
+    //function that recommends clothing based on that the temperature is outside
+
+var temperatureClothing = function(temp_f){
+        if (temp_f >= 90){
+            return 'lightweight clothing, shorts, t-shirt, tank top, sandals, stay indoors in air conditioning when possible';
+        }
+        else if (temp_f >= 80 && temp_f < 90){
+            return 'lightweight clothing, shorts, t-shirt, tank top, sandals';
+        }
+        else if (temp_f >= 65 && temp_f < 80){
+            return 'pants, short or long sleeved shirt, light jacket or sweater, shoes or sandals';
+        }
+        else if (temp_f >= 55 && temp_f < 65){
+            return 'pants, long sleeved shirt, light jacket or sweater, shoes';
+        }
+        else if (temp_f >= 40 && temp_f < 55){
+            return 'long pants, long sleeved shirt, jacket or sweater, shoes';
+        }
+        else if (temp_f >= 30 && temp_f < 40){
+            return 'long pants,long sleeved shirt, pullover, jacket, shoes or boots';
+        }
+        else if (temp_f >= 15 && temp_f < 30){
+            return 'long pants,long sleeved shirt, pullover, jacket, shoes or boots, scarf, mittens, tuque';
+        }
+        else if (temp_f >= 0 && temp_f < 15){
+            return 'long pants,long sleeved shirt, pullover, heavy jacket, warm boots, scarf, gloves, tuque';
+        }
+        else {
+            return 'long and very warm pants,long sleeved shirt, pullover, sweater, heavy jacket, warm boots, socks, scarf, gloves, tuque, stay indoors in well heated areas when possible';
+        };
+            
+            
+}
+
+    var x = document.getElementById("demo");
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            /* navigator.geolocation.getCurrentPosition(showPosition);*/
+            navigator.geolocation.getCurrentPosition(getWeather);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+
+    function getWeather(position) {
+
+        /* including variables and calling getMain for getWeather.js */
+
+            var currLong = position.coords.longitude;
+            var currLat = position.coords.latitude;
+            getMain(currLong, currLat);
+            var returnlat = function(){
+                return currLat;
+            }
+            var returnlong = function(){
+                return currLong;
+            }
+
+        /*--------- end of intrusion ----------*/
+
+        //x.innerHTML = "Latitude: " + position.coords.latitude +
+        //    "<br>Longitude: " + position.coords.longitude;
+        jQuery(document).ready(function($) {
+            $.ajax({
+                url: "https://api.wunderground.com/api/6887efd8390c8e5e/geolookup/conditions/q/" +
+                    position.coords.latitude + "," + position.coords.longitude + ".json",
+                dataType: "jsonp",
+                success: function(parsed_json) {
+                    var city = parsed_json['location']['city'];
+                    var st = parsed_json['location']['state'];
+                    var temp_f = parsed_json['current_observation']['temp_f'];
+                    var weather = parsed_json['current_observation']['weather'];
+
+                    $('#whatToWear').append(weatherAccessories(weather));
+                    $('#whatToWear').append('<br> ' +temperatureClothing(temp_f));
+                }
+            });
+        });
+    }
+
